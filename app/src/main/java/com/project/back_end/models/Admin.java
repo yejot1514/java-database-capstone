a982914d53a34974487a8aa52b0cfa87bc1f9c83
yejot1514 <banchihai@gmail.com>
2025-09-09 12:38:03 -0400

    first commit for admin entity

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -1,5 +1,11 @@
 package com.project.back_end.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.lang.annotation.Inherited;
+
+
+@Entity
 public class Admin {
 
 // @Entity annotation:
@@ -33,5 +39,33 @@ public class Admin {
 
 // 5. Getters and Setters:
 //    - Standard getter and setter methods are provided for accessing and modifying the fields.
+      @Id 
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
 
+      @NotNull(message = "username cannot be null")
+      private String username;
+
+      @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+      @NotNull(message = "password cannot be null")
+      private String password;
+      
+      public void setId(Long id){
+        this.id = id;
+      }
+      public Long getId(){
+        return id;
+      }
+      public void setUsername(String username)    {
+        this.username = username;
+      }
+      public String getUsername(){
+        return username;
+      }
+      public void setPassword(String password){
+        this.password = password;
+      }
+      public String getPassord(){
+        return password;
+      }
 }
